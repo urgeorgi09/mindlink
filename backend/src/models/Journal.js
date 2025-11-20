a1acaf39ee16f90a1ac910882d5beaeb57dc1d45
urgeorgi09 <gogourumov123@abv.bg>
2025-11-20 20:37:25 +0200

    Обновяване на файлове

--- backend/src/models/Journal.js
+++ backend/src/models/Journal.js
@@ -1,1 +1,24 @@
-null
+import mongoose from "mongoose";
+
+const JournalSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+const Journal = mongoose.model("Journal", JournalSchema);
+
+export default Journal;
